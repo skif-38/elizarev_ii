c550d0cfd8895748e856c5208bc29e23b16ea3fc
skif-38 <medvedpro24@gmail.com>
2026-09-22 15:57:26 +0800

    _

--- verb+adverb/verb+adverb.py
+++ verb+adverb/verb+adverb.py
@@ -1,5 +1,5 @@
 import re
-pat = re.compile(r'\b\w+[ое]\s\w+(?:[л]\w*|[тч][ьи][ся]*)\b|\b\w+(?:[л]\w*|[тч][ьи][ся]*)\s\w+[ое]\b')
+pat = re.compile(r'\b\w+[ое]\s\w+(?:л\w*|[тч][ьи]ся?)\b|\b\w+(?:л\w*|[тч][ьи]ся?)\s\w+[ое]\b')
 
 with open("a.txt", encoding="utf-8") as f:
     text = f.read()
